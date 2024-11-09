970a602aab6ae35197523cb810c866ba9ebdd584
Jan Wolf <jan.wolf@ssrd.io>
2024-11-09 16:52:22 +0100

    added template

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
-const express = require('express')
+import {createServer} from 'http';
+import express from 'express';
+import {connectMongoDb} from './database/connection';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 const app = express()
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+connectMongoDb();
+
+export const startServer = () => {
+	// Setup HTTP server
+	const httpServer = createServer(app);
+	console.log('Listening...');
+	httpServer.listen(3000);
+	return httpServer;
+};
+
+export default app;
+
+if (process.env.NODE_ENV !== 'test') {
+	startServer();
+}

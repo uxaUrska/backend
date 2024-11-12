3cdea2b170f1350ea1948b13a7efcef6162d9ae6
wolfe <jan.wolf9@gmail.com>
2024-11-12 20:26:48 +0100

    modified models

--- src/models/zaposlen.model.ts
+++ src/models/zaposlen.model.ts
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const zaposleniSchema = new mongoose.Schema({
     id: {type: String},
-  ime: { type: String, required: true },
-  priimek: { type: String, required: true },
-  vloga: { type: String, enum: ['zaposleni', 'direktor', 'finančni analitik', 'marketinški analitik'], required: true },
-  email: { type: String, required: true, unique: true },
-  stroški: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Strošek' }]
+    ime: { type: String, required: true },
+    priimek: { type: String, required: true },
+    vloga: { type: String, enum: ['zaposleni', 'direktor', 'finančni analitik', 'marketinški analitik'], required: true },
+    email: { type: String, required: true, unique: true },
+    stroški: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Strošek' }]
 });
 
 const Zaposleni = mongoose.model('Zaposleni', zaposleniSchema);
